5c7e0f0e1186366f2aab401f322189c22af20248
Kirnic21 <marcelokirnic@gmail.com>
2023-11-01 13:01:35 -0300

    added media query

--- src/Frente.jsx
+++ src/Frente.jsx
@@ -7,10 +7,10 @@ const Frente = () => {
       <img className="imagem" src={ss}></img>
       <div className='texto'>
       <h2 className="titulo">Titulo</h2>
-      <h3 className='descricao'>
+      <p className='descricao'>
       A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.
       
-</h3>
+</p>
 </div>
     </div>
   );
