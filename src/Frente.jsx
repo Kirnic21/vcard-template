46dfe4aee862e6a0c467c480e286d29966d1acc3
Kirnic21 <marcelokirnic@gmail.com>
2023-11-01 11:09:37 -0300

    correcao de bug

--- src/Frente.jsx
+++ src/Frente.jsx
@@ -5,10 +5,13 @@ const Frente = () => {
   return (
     <div className="frente">
       <img className="imagem" src={ss}></img>
+      <div className='texto'>
       <h2 className="titulo">Titulo</h2>
-      <h3 className='descricao'>A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.
+      <h3 className='descricao'>
+      A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.A noite caía sobre a cidade, e as luzes das ruas começavam a piscar, criando um cenário mágico. As pessoas apressavam-se nas calçadas, cada uma com sua própria história e destino. O ar estava carregado de expectativa, como se algo extraordinário estivesse prestes a acontecer.
+      
 </h3>
-
+</div>
     </div>
   );
 }
